1791ef13a2357a48ca166f921728cc1ce92e05ef
Ali Burzhi <aliburzhi033@gmail.com>
2021-05-25 15:29:21 +0300

    задачи 2.1 2.2 2.3

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-// ЗАДАЧА 1
-const numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", 100);
+ // ЗАДАЧА 1.1
+ const numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", 100);
 console.log(numberOfFilms);
 
-// ЗАДАЧА 2
+// ЗАДАЧА 1.2
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -12,15 +12,29 @@ const personalMovieDB = {
     privat: false
 };
 
-// ЗАДАЧА 3
+// ЗАДАЧА 1.3
 
-const a = prompt("Один из просмотренных фильмов?", ""),
-    b = prompt("На сколько оцените его?", ""),
-    c = prompt("Один из просмотренных фильмов?", ""),
-    d = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// ЗАДАЧА 2.1
+
+for (let i = 0; i < 2; i++) {
+    
+        const a = prompt("Один из просмотренных фильмов?", ""),
+        b = prompt("На сколько оцените его?", "");
+   
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+         personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+    
+}
+if (personalMovieDB.count < 10) {
+    console.log("MALO FILMOV");
+} else if (personalMovieDB > 10 && personalMovieDB < 30) {
+    console.log("NORM FILMOV");
+} else {
+    console.log("KINOMAN");
+};
 
 console.log(personalMovieDB);
-
